ebc08cbfa4525e3a96243154144312f1ad960d3b
koresachin7 <koresachin7@gmail.com>
2021-12-08 12:19:14 +0530

    Added : - Welcome Message

---
+++ linecomparison.py
@@ -0,0 +1,13 @@
+"""
+* @Author: Sachin S Kore
+* @Date: 2021-12-8
+* @Title :  Welcome message
+"""
+
+
+class LineComparison:
+    pass
+
+
+if __name__ == '__main__':
+    print("Welcome To Line Comparison Computation Program")
