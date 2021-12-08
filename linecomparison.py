f95a95ec061175999641bede96387901eee6eec9
koresachin7 <koresachin7@gmail.com>
2021-12-08 12:39:28 +0530

    Added : - Calculate Length of line

--- linecomparison.py
+++ linecomparison.py
@@ -1,13 +1,56 @@
 """
 * @Author: Sachin S Kore
 * @Date: 2021-12-8
-* @Title :  Welcome message
+* @Title : Calculate length of Line
 """
+import math
 
 
 class LineComparison:
-    pass
+    def __init__(self):
+        # using list to store player class object
+        self.line_list = []
+        self.length_of_line = 0
+
+    def length_find(self):
+        """
+        Description:
+            this function for Calculate length of line
+        """
+        for i in self.line_list:
+            self.length_of_line = math.sqrt((i.x2 - i.x1) * (i.x2 - i.x1) + (i.y2 - i.y1) * (i.y2 - i.y1))
+            print("Length of Line :-  ", self.length_of_line)
+
+
+class Line:
+    # parameterized constructor
+    def __init__(self, x1, x2, y1, y2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
 
 
 if __name__ == '__main__':
     print("Welcome To Line Comparison Computation Program")
+    line_comparison = LineComparison()
+    """
+    Description:
+        * Creating multiple Object multiple line length calculation
+        * using x and y co_ordinates for that 
+    """
+    lines = int(input("Number of line Comparison : --""\n"))
+    for i in range(lines):
+        print("Enter co-ordinates for x-axis of first line : ")
+        x1 = int(input("line x1""\n"))
+        x2 = int(input("line x2""\n"))
+        print("Enter co-ordinates for y-axis of first line : ")
+        y1 = int(input("line y1""\n"))
+        y2 = int(input("line y2""\n"))
+        # creating object of the class
+        line_obj = Line(x1, x2, y1, y2)
+        # object append (adding) To line
+        line_comparison.line_list.append(line_obj)
+    # calling the instance method using the object line_comparison
+    line_comparison.length_find()
+
